0b3522a2b8e887ae57710ed09992f70dee4a1dda
Louie <lnavarro6@dxc.com>
2021-03-13 20:53:54 +0800

    Fix handler and local server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,54 @@
 import serverless from "serverless-http";
+import express from "express";
+import cors from "cors";
 
-import { app } from "server/app";
-import { attachPrivateRoutes } from "./server/routes";
+import { addRespondToResponse } from "middleware/response";
+import { handleError } from "middleware/errors";
+import { RouteNotFoundError } from "errors";
+
+import { createDatabaseConnection } from "database/createConnection";
+import { attachPrivateRoutes } from "routes";
+
+const establishDatabaseConnection = async (): Promise<void> => {
+  try {
+    await createDatabaseConnection();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const initializeExpress = (): void => {
+  app.use(cors());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use(addRespondToResponse);
+
+  app.use(
+    isServerless ? "/.netlify/functions/index" : "/",
+    attachPrivateRoutes()
+  );
+
+  app.use((req, _res, next) => next(new RouteNotFoundError(req.originalUrl)));
+  app.use(handleError);
+};
+
+const initializeApp = async (): Promise<void> => {
+  await establishDatabaseConnection();
+  initializeExpress();
+};
+
+let isServerless = false;
+const app = express();
 
 export const handler = async (event: any, context: any) => {
-  app.use("/.netlify/functions/index", attachPrivateRoutes());
-  return await serverless(app)(event, context);
+  isServerless = true;
+  await initializeApp();
+  return serverless(app)(event, context);
+};
+
+export const runLocalServer = () => {
+  initializeApp();
+  const port = process.env.PORT || 5200;
+  app.listen(port, () => console.log(`listening at port ${port}`));
 };
--- src/local.ts
+++ src/local.ts
@@ -1,10 +1,6 @@
 import "module-alias/register";
 import "dotenv/config";
 
-import { app } from "server/app";
-import { attachPrivateRoutes } from "server/routes";
+import { runLocalServer } from "./index";
 
-app.use("/", attachPrivateRoutes());
-
-const port = process.env.PORT || 5000;
-app.listen(() => console.log(`listening at port ${port}`));
+runLocalServer();
