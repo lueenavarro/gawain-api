48e15a44cec9a684af95c44ca83cdb0798da030d
lueenavarro <47080916+lueenavarro@users.noreply.github.com>
2021-03-25 04:13:16 +0800

    Fix server local

--- server-local.ts
+++ server-local.ts
@@ -1,11 +1,8 @@
 import "module-alias/register";
 import "dotenv/config";
 
-import {app, initializeApp} from "./src/server/app";
+import { app, initializeApp } from "./src/server/app";
 
-export const runLocalServer = () => {
-    initializeApp();
-    const port = process.env.PORT || 5000;
-    app.listen(port, () => console.log(`listening at port ${port}`));
-  };
-  
+initializeApp();
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`listening at port ${port}`));
