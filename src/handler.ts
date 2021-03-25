dfa9a6b3721db6dfabb855d75fd9d146c4dbfdc0
lueenavarro <47080916+lueenavarro@users.noreply.github.com>
2021-03-25 17:48:00 +0800

    Fix api

--- src/handler.ts
+++ src/handler.ts
@@ -3,6 +3,6 @@ import serverless from "serverless-http";
 import { app, initializeApp } from "server/app";
 
 export const handler = async (event: any, context: any) => {
-    await initializeApp("/.netlify/functions/index");
+    await initializeApp("/.netlify/functions/handler");
     return serverless(app)(event, context);
   };
