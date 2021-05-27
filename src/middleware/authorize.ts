75e9efe9271277e32778026dfce1d739b4762529
Louie <louie.navarro@outlook.com>
2021-05-27 09:20:16 +0800

    Catch no access token provided error

--- src/middleware/authorize.ts
+++ src/middleware/authorize.ts
@@ -1,8 +1,12 @@
-import { catchErrors } from "errors"
-import token from "utils/token"
+import { catchErrors, InvalidTokenError } from "errors";
+import token from "utils/token";
 
 export const authorize = catchErrors((req, _res, next) => {
+  try {
     const user = token.verifyAccessToken(req.signedCookies.accessToken);
     req.user = user;
     next();
-})
+  } catch (error) {
+    throw new InvalidTokenError();
+  }
+});
